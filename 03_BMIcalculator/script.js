fcf8aeaadb17de5070a24b57d78007fd1f4108bc
NARESH KUMAR <142076994+nareshkumarhp14s@users.noreply.github.com>
2024-02-26 15:04:32 +0530

    Add files via upload

---
+++ 03_BMIcalculator/script.js
@@ -0,0 +1,38 @@
+let form = document.querySelector('form');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const height = parseInt(document.querySelector("#height").value);
+    const weight = parseInt(document.querySelector("#weight").value);
+    const resultElement = document.querySelector("#results");
+    const under = document.querySelector("#under");
+    const normal = document.querySelector("#normal");
+    const over = document.querySelector("#over");
+
+    if (height === '' || height < 0 || isNaN(height)) {
+        resultElement.innerHTML = `Please enter a valid height: ${height}`;
+    }
+    else if (weight === '' || weight < 0 || isNaN(weight)) {
+        resultElement.innerHTML = `Please enter a valid weight: ${weight}`;
+    }
+    else {
+        const Bmi = (weight / ((height * height) / 10000)).toFixed(2);
+        resultElement.innerHTML = `<span>${Bmi}</span>`;
+        setColor(Bmi);
+    }
+
+    function setColor(Bmi) {
+        if (Bmi <= 18.6) {
+            under.style.color = 'blue';
+            normal.style.color = ''; // Reset other styles
+            over.style.color = ''; // Reset other styles
+        } else if (Bmi >= 24.9) {
+            over.style.color = 'red';
+            normal.style.color = ''; // Reset other styles
+            under.style.color = ''; // Reset other styles
+        } else {
+            normal.style.color = 'green';
+            under.style.color = ''; // Reset other styles
+            over.style.color = ''; // Reset other styles
+        }
+    }
+});
